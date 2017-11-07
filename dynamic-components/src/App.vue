0d844abc246a119199d5a56deb9ca33c010c03f5
Fabricio Campos <fabricio@vizir.com.br>
2017-11-06 22:59:41 -0200

    [Feat] add dynamic components project

---
+++ dynamic-components/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <p v-if="selectedComponent === 'appBlue'">Blue content</p>
+                    <p v-if="selectedComponent === 'appGreen'">Green content</p>
+                    <p v-if="selectedComponent === 'appRed'">Red content</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: () => {
+            return {
+                selectedComponent: ''
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
